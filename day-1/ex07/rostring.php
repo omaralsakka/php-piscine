2820fd21f97bf0826f2e221f814016106a177d0f
Omar Alsakka <djomrofficial@gmail.com>
2022-03-17 18:09:13 +0200

    final ver

---
+++ day-1/ex07/rostring.php
@@ -0,0 +1,17 @@
+#!/usr/bin/php
+<?php
+	if ($argc > 1)
+	{
+		$tmp = trim($argv[1]);
+		if (strpos($tmp, ' '))
+		{
+			$str = preg_replace('/\s\s+/', " ", $tmp);
+			$arr = explode(" ", $str);
+			$str = array_shift($arr);
+			array_push($arr, $str);
+			echo implode(" ",$arr) . "\n";
+		}
+		else
+			echo "$argv[1]\n";
+	}
+?>
