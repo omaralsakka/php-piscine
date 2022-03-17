51921288dab75d86d8e07baf72f85119aea64e87
Omar Alsakka <djomrofficial@gmail.com>
2022-03-17 12:43:16 +0200

    ex02 done

---
+++ day-1/ex02/oddeven.php
@@ -0,0 +1,26 @@
+#!/usr/bin/php
+<?php
+	
+	while (1)
+	{
+		echo "Enter a number: ";
+		
+		if ($nb = fgets(STDIN))
+		{
+			$nb = trim($nb);
+			if (!(is_numeric($nb)))
+				echo "'$nb' is not a number";
+			$res = intval($nb);
+			if ($res % 2 == 1)
+				echo "The number $res is odd";
+			else
+				echo "The number $res is even";
+			echo "\n";
+		}
+		else
+		{
+			echo "\n";
+			exit (0);
+		}
+	}
+?>
