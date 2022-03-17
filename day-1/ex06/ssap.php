731a21df891031187115ebd0253cbc4abe738297
Omar Alsakka <djomrofficial@gmail.com>
2022-03-17 20:14:40 +0200

    fixed extra space issue

--- day-1/ex06/ssap.php
+++ day-1/ex06/ssap.php
@@ -1,22 +1,22 @@
 #!/usr/bin/php
-
 <?php
 	$i = 0;
 	$x = 0;
-	//$arr = array();
+	$arr = array();
 	if ($argc < 2)
 		exit (0);
 	while (++$i < $argc)
 	{
-		$str = preg_replace('/\s\s+/', ' ', $argv[$i]);
-		if (strpos($str, " "))
+		$str = preg_replace('/\s\s+/', ' ',  $argv[$i]);
+		$trm = trim($str);
+		if (strpos($trm, " "))
 		{
-			$arr2 = explode(' ', $str);
+			$arr2 = explode(' ', $trm);
 			$arrtemp = $arr;
 			$arr = array_merge($arrtemp, $arr2);
 		}
 		else
-			array_push($arr, $str);
+			array_push($arr, $trm);
 	}
 	sort($arr);
 	while ($x < count($arr))
