60754afbfd11956e576208b88a2f6fd6062427fb
Omar Alsakka <djomrofficial@gmail.com>
2022-03-19 11:30:22 +0200

    in-process

---
+++ day-2/ex01/one_more_time.php
@@ -0,0 +1,32 @@
+#!/usr/bin/php
+<?php
+    date_default_timezone_set('Europe/Paris');
+    
+	$week = array(
+		1 => "lundi",
+		2 => "mardi",
+		3 => "mercredi",
+		4 => "jeudi",
+		5 => "vendredi",
+		6 => "samedi",
+		7 => "dimanche");
+
+	$month = array(
+		1 => "janvier",
+		2 => "février",
+		3 => "mars",
+		4 => "avril",
+		5 => "mai",
+		6 => "juin",
+		7 => "juillet",
+		8 => "août",
+		9 => "septembre",
+		10 => "octobre",
+		11 => "novembre",
+		12 => "décembre");
+	
+	if ($argc < 2)
+        exit();
+
+    $date = explode(" ", $argv[1]);
+?>
