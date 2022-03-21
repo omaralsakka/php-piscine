fdd2efba49ac6b910c6a379b54aa186dbfd952e9
Omar Alsakka <djomrofficial@gmail.com>
2022-03-21 11:33:48 +0200

    working ex

--- day-2/ex01/one_more_time.php
+++ day-2/ex01/one_more_time.php
@@ -1,8 +1,14 @@
 #!/usr/bin/php
 <?php
-    date_default_timezone_set('Europe/Paris');
-    
-	$week = array(
+function ft_wrong()
+{
+	echo "Wrong Format\n";
+	exit ();
+}
+
+	date_default_timezone_set('Europe/Paris');
+	
+	$weeks = array(
 		1 => "lundi",
 		2 => "mardi",
 		3 => "mercredi",
@@ -11,7 +17,7 @@
 		6 => "samedi",
 		7 => "dimanche");
 
-	$month = array(
+		$months = array(
 		1 => "janvier",
 		2 => "février",
 		3 => "mars",
@@ -25,8 +31,23 @@
 		11 => "novembre",
 		12 => "décembre");
 	
-	if ($argc < 2)
-        exit();
-
-    $date = explode(" ", $argv[1]);
+	if ($argc == 2)
+	{
+		$date = explode(" ", $argv[1]);
+		if (count($date) != 5 ||
+			preg_match("/^[1-9]$|0[1-9]|[1-2][0-9]|3[0-1]$/", $date[1], $date[1]) === 0 ||
+			preg_match("/^[0-9]{4}$/", $date[3], $date[3]) === 0 ||
+			preg_match("/^([0-1][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$/", $date[4], $date[4]) === 0) 
+				ft_wrong();
+		$date[0] = array_search(lcfirst($date[0]), $weeks);
+		$date[2] = array_search(lcfirst($date[2]), $months);
+		if (!$date[0] || !$date[2])
+			ft_wrong();
+		print_r ($date);
+		$result = mktime($date[4][1], $date[4][2], $date[4][3], $date[2], $date[1][0], $date[3][0]);
+		if (date( "N", $result) == $date[0])	
+			echo $result."\n";
+		else
+			ft_wrong();
+	}
 ?>
