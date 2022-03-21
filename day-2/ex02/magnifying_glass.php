9fe4fe31c4981f38852e72f33035d150d8021d90
Omar Alsakka <djomrofficial@gmail.com>
2022-03-21 14:12:44 +0200

    working ex

---
+++ day-2/ex02/magnifying_glass.php
@@ -0,0 +1,32 @@
+#!/usr/bin/php
+<?php
+function ft_up($match)
+{
+	return strtoupper($match[0]);
+}
+
+function ft_filter($match)
+{
+	
+	$pattern = "/\".*?\"|>.*?</";
+	$result = preg_replace_callback($pattern, 'ft_up', $match[0]);
+	return ($result);
+}
+
+function ft_catch($match)
+{
+	
+	$pattern = "/(title=.*?<)|>.*?</";
+	$result = preg_replace_callback($pattern, 'ft_filter', $match[0]);
+	return ($result);
+}
+	if ($argc < 2 || !file_exists($argv[1]))
+		exit ();
+	$file = fopen($argv[1], 'r');
+	$page = "";
+	$pattern = "/<a.*</";
+	while (!feof($file))
+		$page .= fgets($file);
+	$result = preg_replace_callback($pattern, 'ft_catch', $page);
+	echo $result;
+?>
