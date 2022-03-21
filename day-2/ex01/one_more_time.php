c9d69243aef12a029dc3e8fabae700408a8aa859
Omar Alsakka <djomrofficial@gmail.com>
2022-03-21 14:14:12 +0200

    fixed var name and deleted one print_r

--- day-2/ex01/one_more_time.php
+++ day-2/ex01/one_more_time.php
@@ -8,7 +8,7 @@ function ft_wrong()
 
 	date_default_timezone_set('Europe/Paris');
 	
-	$weeks = array(
+	$days = array(
 		1 => "lundi",
 		2 => "mardi",
 		3 => "mercredi",
@@ -17,7 +17,7 @@ function ft_wrong()
 		6 => "samedi",
 		7 => "dimanche");
 
-		$months = array(
+	$months = array(
 		1 => "janvier",
 		2 => "février",
 		3 => "mars",
@@ -39,11 +39,10 @@ function ft_wrong()
 			preg_match("/^[0-9]{4}$/", $date[3], $date[3]) === 0 ||
 			preg_match("/^([0-1][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$/", $date[4], $date[4]) === 0) 
 				ft_wrong();
-		$date[0] = array_search(lcfirst($date[0]), $weeks);
+		$date[0] = array_search(lcfirst($date[0]), $days);
 		$date[2] = array_search(lcfirst($date[2]), $months);
 		if (!$date[0] || !$date[2])
 			ft_wrong();
-		print_r ($date);
 		$result = mktime($date[4][1], $date[4][2], $date[4][3], $date[2], $date[1][0], $date[3][0]);
 		if (date( "N", $result) == $date[0])	
 			echo $result."\n";
