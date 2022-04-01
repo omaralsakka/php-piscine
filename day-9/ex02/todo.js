cc1f15483f0278863db46205e224f9f584cd04eb
Omar Alsakka <djomrofficial@gmail.com>
2022-04-01 18:28:07 +0300

    final version

--- day-9/ex02/todo.js
+++ day-9/ex02/todo.js
@@ -1,36 +1,31 @@
 var idx = 0;
-console.log(document.cookie);
+
 if (document.cookie && document.cookie != ""){
 	var Arr = document.cookie.split(';');
 	for (var k = 0; k < Arr.length; k++){
 		var values = Arr[k].split("=");
-		var dv = document.createElement("DIV");
-		dv.setAttribute('id', k);
-		dv.setAttribute('onclick', 'removeItem(this.id)');
-		var tx = document.createTextNode(values[1]);
-		var ls = document.createElement("li");
-		ls.appendChild(tx);
-		dv.appendChild(ls);
-		var ftList = document.getElementById("ft_list");
-		ftList.insertBefore(dv, ftList.children[0]);
+		listing(values[1], k);
 	}
 	idx = k;
 }
 
+function listing(textValue, ix){
+	var dv = document.createElement("DIV");
+	dv.setAttribute('id', ix);
+	dv.setAttribute('onclick', 'removeItem(this.id)');
+	var tx = document.createTextNode(textValue);
+	var ls = document.createElement("li");
+	ls.appendChild(tx);
+	dv.appendChild(ls);
+	var ftList = document.getElementById("ft_list");
+	ftList.insertBefore(dv, ftList.children[0]);
+}
+
 function addItem(){
-	console.log(idx);
 	let toDo = prompt("Please enter a to do item");
 	if (toDo != null){
 		if (toDo.length){
-			const li = document.createElement("li");
-			const txt = document.createTextNode(toDo);
-			li.appendChild(txt);
-			const div = document.createElement("DIV");
-			div.setAttribute('id', idx);
-			div.setAttribute('onclick', 'removeItem(this.id)');
-			div.appendChild(li);
-			const list = document.getElementById("ft_list");
-			list.insertBefore(div, list.children[0]);
+			listing(toDo, idx);
 			var nwCookie = idx + "=" + toDo + 
 			"; expires=Thu, 18 Dec 2050 12:00:00 UTC; secure; path=/;";
 			idx++;
