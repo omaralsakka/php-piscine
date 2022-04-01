de1cdeb4e178ec50bdfe54e73e12baaaa858003b
Omar Alsakka <djomrofficial@gmail.com>
2022-04-01 18:21:27 +0300

    done ex

--- day-9/ex02/todo.js
+++ day-9/ex02/todo.js
@@ -1,29 +1,41 @@
 var idx = 0;
-let newcookie;
-function createCookie(value){
-	if (!newcookie){
-		document.cookie = 'list=' + value;
+console.log(document.cookie);
+if (document.cookie && document.cookie != ""){
+	var Arr = document.cookie.split(';');
+	for (var k = 0; k < Arr.length; k++){
+		var values = Arr[k].split("=");
+		var dv = document.createElement("DIV");
+		dv.setAttribute('id', k);
+		dv.setAttribute('onclick', 'removeItem(this.id)');
+		var tx = document.createTextNode(values[1]);
+		var ls = document.createElement("li");
+		ls.appendChild(tx);
+		dv.appendChild(ls);
+		var ftList = document.getElementById("ft_list");
+		ftList.insertBefore(dv, ftList.children[0]);
 	}
-	newcookie += document.cookie;
-	document.cookie = 'list=' + newcookie;
+	idx = k;
 }
 
 function addItem(){
+	console.log(idx);
 	let toDo = prompt("Please enter a to do item");
-	if (toDo.length){
-		createCookie(toDo)
-		const li = document.createElement("li");
-		const txt = document.createTextNode(toDo);
-		li.appendChild(txt);
-		const div = document.createElement("DIV");
-		div.setAttribute('id', idx);
-		div.setAttribute('onclick', 'removeItem(this.id)');
-		div.appendChild(li);
-		const list = document.getElementById("ft_list");
-		list.insertBefore(div, list.children[0]);
-		idx++;
-		//append adds at the end of the list
-		// document.getElementById("ft_list").appendChild(div);
+	if (toDo != null){
+		if (toDo.length){
+			const li = document.createElement("li");
+			const txt = document.createTextNode(toDo);
+			li.appendChild(txt);
+			const div = document.createElement("DIV");
+			div.setAttribute('id', idx);
+			div.setAttribute('onclick', 'removeItem(this.id)');
+			div.appendChild(li);
+			const list = document.getElementById("ft_list");
+			list.insertBefore(div, list.children[0]);
+			var nwCookie = idx + "=" + toDo + 
+			"; expires=Thu, 18 Dec 2050 12:00:00 UTC; secure; path=/;";
+			idx++;
+			document.cookie = nwCookie;
+		}
 	}
 }
 
@@ -31,5 +43,8 @@ function removeItem(clicked_id){
 	if (confirm("Confirm to remove the \"TO DO item?\"")){
 		element = document.getElementById(clicked_id);
 		element.remove();
+		document.cookie = clicked_id + 
+		"=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+		console.log(document.cookie);
 	}
 }
