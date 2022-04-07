987c507c0fa83b78c86e6b94a727aec223ec7715
oabdelfa <djomrofficial@gmail.com>
2022-04-07 16:42:46 +0300

    added str_pad, check for dates also

--- day-2/ex03/who.php
+++ day-2/ex03/who.php
@@ -10,9 +10,11 @@
 			$data = unpack("a256user/a4id/a32line/ipid/itype/itime", $data);
 			if ($data['type'] == 7)
 			{
-				echo trim($data['user']) . " ";
-				echo trim($data['line']) . "  ";
+				$usr = trim($data['user']);
+				$dt = trim($data['line']);
 				$time = date("M  j H:i", $data['time']);
+				echo str_pad($usr,9," ");
+				echo str_pad($dt,9," ");
 				echo $time . " \n";		
 			}
 		}
