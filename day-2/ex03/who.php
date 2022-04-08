dadd8a757fbf384d6574e4a897753a546d7af2ad
oabdelfa <djomrofficial@gmail.com>
2022-04-08 14:44:27 +0300

    added printf with precisions

--- day-2/ex03/who.php
+++ day-2/ex03/who.php
@@ -4,18 +4,18 @@
 	date_default_timezone_set("Europe/Helsinki");
 	while (!feof($file))
 	{
-		$data = fread($file, 628);
-		if (strlen($data) == 628)
+		$fd = fread($file, 628);
+		if (strlen($fd) == 628)
 		{
-			$data = unpack("a256user/a4id/a32line/ipid/itype/itime", $data);
-			if ($data['type'] == 7)
+			$fd = unpack("a256user/a4id/a32line/ipid/itype/itime", $fd);
+			if ($fd['type'] == 7)
 			{
-				$usr = trim($data['user']);
-				$dt = trim($data['line']);
-				$time = date("M  j H:i", $data['time']);
-				echo str_pad($usr,9," ");
-				echo str_pad($dt,9," ");
-				echo $time . " \n";		
+				$usr = trim($fd['user']);
+				$dt = trim($fd['line']);
+				$month = date("M", $fd['time']);
+				$day = date("j", $fd['time']);
+				$time = date("H:i", $fd['time']);
+				printf("%-8s %-8s %s %2s %s\n", $usr, $dt, $month, $day, $time);
 			}
 		}
 	}
